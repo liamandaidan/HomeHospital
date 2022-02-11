ab563790d927929836b81446dabbb95c9b720e7c
Bushra Osman <84491178+bushx@users.noreply.github.com>
2022-02-11 11:27:06 -0700

    Create Patient.Schema.js
    added basic patient schema

---
+++ HomeHospital-Backend/src/models/Patient.Schema.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const patientSchema = new mongoose.Schema({
+	patientID: {
+		type: int,
+		required: true,
+	},
+	HCnumber: {
+		type: String,
+		required: true
+	},
+	phoneNumber: {
+		type: String,
+		required: true
+	},
+    gender:{
+        type: char,
+        required: true
+    },
+    dateOfBirth:{
+        type: Date,
+        required: true
+    }
+})
+
+export default mongoose.model('Hospital', hospitalSchema)
