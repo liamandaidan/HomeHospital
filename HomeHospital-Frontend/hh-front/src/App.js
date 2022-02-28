4836d519abc89523d3efc9f8038e7d3e3d5d1caa
rawbz <rmjbalanag@gmail.com>
2022-02-28 09:07:41 -0700

    Updated Severity function

--- HomeHospital-Frontend/hh-front/src/App.js
+++ HomeHospital-Frontend/hh-front/src/App.js
@@ -1,48 +1,52 @@
-import React from 'react';
-import SymptomsForm from './components/SymptomsForm.js';
-import Navigation from './components/Navigation.js';
-import PatientInfo from './components/PatientInfo.js';
+import React from "react";
+import SymptomsForm from "./components/SymptomsForm.js";
+import Navigation from "./components/Navigation.js";
+import PatientInfo from "./components/PatientInfo.js";
 
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/main.css';
-
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/main.css";
 
 function App() {
   return (
     <>
       <Navigation />
-      <Container>
-    <Row>
-      <Col>
-        <Breadcrumb>
-        <Breadcrumb.Item href='#'>Home</Breadcrumb.Item>
-        <Breadcrumb.Item href='https://getbootstrap.com/docs/4.0/components/breadcrumb/'>
-          Hospital
-        </Breadcrumb.Item>
-        <Breadcrumb.Item active>Symptoms</Breadcrumb.Item>
-      </Breadcrumb>
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        <ProgressBar now={60} />
-      </Col>
-      <Col>
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        <SymptomsForm />
-      </Col>
-      <Col>
-      <PatientInfo />
-      </Col>
-    </Row>
+      <Container className="main-container">
+        <Row>
+          <Col>
+          <div className="breadcrumb-div">
+            <Breadcrumb>
+              <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+              <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+                Hospital
+              </Breadcrumb.Item>
+              <Breadcrumb.Item active>Symptoms</Breadcrumb.Item>
+            </Breadcrumb>
+          </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          <div className="progress-div">
+            <ProgressBar now={60} />
+          </div>
+          </Col>
+          <Col></Col>
+        </Row>
+        <Row>
+          <Col>
+          <div className="symptoms-div">
+            <SymptomsForm />
+          </div>
+          </Col>
+          <Col>
+            <PatientInfo />
+          </Col>
+        </Row>
       </Container>
     </>
   );
--- HomeHospital-Frontend/hh-front/src/components/SymptomsForm.js
+++ HomeHospital-Frontend/hh-front/src/components/SymptomsForm.js
@@ -1,34 +1,43 @@
-import React, { useState } from 'react';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
-import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import InputGroup from 'react-bootstrap/InputGroup';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/SymptomForm.css';
+import React, { useState } from "react";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import InputGroup from "react-bootstrap/InputGroup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/SymptomForm.css";
+import DropdownMenu from "react-bootstrap/esm/DropdownMenu";
 
 function SymptomsForm() {
-  const [symptom, setSymptom] = useState('');
-  const [severity, setSeverity] = useState(0);
+  const [symptom, setSymptom] = useState("");
+  const [severity, setSeverity] = useState();
+
+  const severityOptions = [
+    { key: 1, text: '1', value: 1 },
+    { key: 2, text: '2', value: 2 },
+    { key: 3, text: '3', value: 3 },
+  ];
 
   const [symptomsList, setSymptomsList] = useState([
     {
-      symptom: '',
-      severity: 0,
+      symptom: "",
+      severity: "",
     },
   ]);
 
   console.log(symptomsList);
+  // console.log(severity);
+
 
   const handleSymptomsAdd = () => {
     setSymptomsList([
       ...symptomsList,
       {
-        symptom: '',
-        severity: 0,
+        symptom: "",
+        severity: "",
       },
     ]);
   };
@@ -48,48 +57,58 @@ function SymptomsForm() {
     setSymptomsList(list);
   };
 
-  const handleSeverityChange = (e) => {
+  const handleSeverityChange = (e, index) => {
+    console.log(index);
     console.log(e);
+
+    const list = [...symptomsList];
+    list[index][e.name] = e;
+    setSymptomsList(list);
   };
 
   return (
-    <Container className='main-container'>
+    <Container className="symptoms-container">
       <Row>
-        <h2>Enter Symtoms</h2>
+        <div className="title-div">
+          <h2>Enter Symtoms</h2>
+        </div>
       </Row>
       <Row>
-        <p>Please enter current symptoms along with severity from 1-5</p>
+        <div className="details-div">
+          Please enter current symptoms along with severity of 1-5
+        </div>
       </Row>
       <Row>
         <Col>
           <Form>
             {symptomsList.map((singleSymptom, index) => (
               <>
-                <InputGroup key={index} className='symptomGroup'>
+                <InputGroup key={index} className="symptomGroup">
                   <Form.Control
-                    type='text'
-                    name='symptom'
-                    placeholder='Enter Symptom'
+                    type="text"
+                    name="symptom"
+                    placeholder="Enter Symptom"
                     value={singleSymptom.symptom.service}
                     onChange={(e) => handleSymptomsChange(e, index)}
                   />
                   <DropdownButton
-                    className='severityDrop'
-                    variant='outline-secondary'
-                    title='Severity'
-                    id='input-group-dropdown-2'
-                    align='end'
-                    name='severity'
-                    onSelect={handleSeverityChange}
+                    className="severityDrop"
+                    variant="outline-secondary"
+                    title="Severity"
+                    id="input-group-dropdown-2"
+                    align="end"
+                    name="severity"
+                    onSelect={(e) => handleSeverityChange(e, index)}
                   >
-                    <Dropdown.Item eventKey='1'>1</Dropdown.Item>
-                    <Dropdown.Item eventKey='2'>2</Dropdown.Item>
-                    <Dropdown.Item eventKey='3'>3</Dropdown.Item>
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
                   </DropdownButton>
+
                   {symptomsList.length > 1 && (
                     <Button
-                      className='remove-btn'
-                      variant='outline-secondary'
+                      className="remove-btn"
+                      variant="outline-secondary"
                       onClick={() => handleSymptomsRemove(index)}
                     >
                       <span>x</span>
@@ -98,7 +117,7 @@ function SymptomsForm() {
                 </InputGroup>
                 {symptomsList.length - 1 === index && symptomsList.length < 5 && (
                   <Button
-                    className='add-btn btn-light'
+                    className="add-btn btn-light"
                     onClick={handleSymptomsAdd}
                   >
                     <span>Add Symptom</span>
@@ -106,9 +125,14 @@ function SymptomsForm() {
                 )}
               </>
             ))}
-            <div className='additional-info'>
+            <div className="additional-info">
               <Form.Label>Additional Information</Form.Label>
-              <Form.Control as='textarea' rows={3} />
+              <Form.Control as="textarea" rows={3} />
+            </div>
+            <div>
+              <Button className="submit-btn btn-light">
+                <span>Submit Symptoms</span>
+              </Button>
             </div>
           </Form>
         </Col>
