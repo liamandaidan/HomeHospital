5d5488cbea4df3fc907f48fad3691fb80fc5cd90
Liam McLaughlin <84680661+liamandaidan@users.noreply.github.com>
2022-03-01 16:06:28 -0700

    Work so far

--- HomeHospital-Frontend/hh-front/src/components/ForgotPass.jsx
+++ HomeHospital-Frontend/hh-front/src/components/ForgotPass.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-import background from "./img/bg.png";
+import { Container } from "react-bootstrap";
 import "./forgotpass.css";
+import logo1 from "./img/hb1.png";
+import logo2 from "./img/hb2.png";
 export default function ForgotPass() {
   return (
-    <div className="bgdiv">
-      <div className="content">
-        <div className="med"></div>
-        <div className="logo1"></div>
-        <div className="right">
-          <div className="logo2"></div>
+    <Container>
+      <div className="bgdiv">
+        <div className="content">
+          <div className="med"/>
+          <div className="logo1">
+            <img src={logo1} className="logo1"></img>
+          </div>
+          <div className="right">
+            <div className="logo2">
+              <img src={logo2} className="logo2"></img>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 }
