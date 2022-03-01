8df4a0ded074efbf272b17985ef891990dc302ff
rawbz <rmjbalanag@gmail.com>
2022-03-01 13:13:49 -0700

    created modal component

--- HomeHospital-Frontend/hh-front/src/App.js
+++ HomeHospital-Frontend/hh-front/src/App.js
@@ -4,6 +4,7 @@ import Navigation from "./components/Navigation.js";
 import PatientInfo from "./components/PatientInfo.js";
 import BreadcrumbNav from "./components/BreadcrumbNav.js";
 import ProgressNav from "./components/ProgressNav.js";
+import AlertModal from "./components/AlertModal.js";
 
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
@@ -38,6 +39,7 @@ function App() {
           </Col>
           <Col>
             <PatientInfo />
+            <AlertModal />
           </Col>
         </Row>
       </Container>
