412faeed94512521b227c9ee9ed6927fdbef54f8
Bushra Osman <84491178+bushx@users.noreply.github.com>
2022-03-03 08:33:22 -0700

    Looped Symptoms Route
    Added For Loop to take in an unknown amount of Symptoms

--- HomeHospital-Backend/src/api/routes/symptoms.route.js
+++ HomeHospital-Backend/src/api/routes/symptoms.route.js
@@ -10,35 +10,37 @@ const app = express.Router();
 app.post("/newSymptom", async (req, res) => {
   const { symptomList, generalDescription } = req.body;
 
+
   try {
     // Loop over symptom list and add
-
-    // add general description
-    // Check to make sure we don't have the symptom already created
-    const result = await VisitRequest.exists({
-      symptom: symptom,
-    });
-
-    if (result?._id) {
-      console.log("Symptoms were already added!!");
-      res.status(400).send({
-        message: "Error",
+    for (let i = 0; i < symptomList.length; i++) {
+      // Check to make sure we don't have the symptom already created
+      const result = await VisitRequest.exists({
+        symptom: symptom,
       });
 
-      return;
+      if (result?._id) {
+        console.log("Symptoms were already added!!");
+        res.status(400).send({
+          message: "Error",
+        });
+
+        return;
+      }
+
+      // create the new symptom and severity
+      const symptoms = await Symptom.create({
+        severity: severity,
+
+        description: description,
+      });
+      symptoms.save();
+      console.log("Symptom added: " + symptoms);
+      res.status(201).send({
+        message: "Symptom Added",
+      });
     }
-
-    // create the new symptom and severity
-    const symptoms = await Symptom.create({
-      severity: severity,
-
-      description: description,
-    });
-    symptoms.save();
-    console.log("Symptom added: " + symptoms);
-    res.status(201).send({
-      message: "Symptom Added",
-    });
+    // add general description
   } catch (error) {
     res.status(400).send({
       message: "Error",
