6db58e6bbfca45bcc9260bef1df1a4b43d1dbcc6
Ridge.Bnz <ridge.banez@edu.sait.ca>
2022-03-03 08:57:26 -0700

    Created SelectHospital component

--- HomeHospital-Frontend/hh-front/src/App.js
+++ HomeHospital-Frontend/hh-front/src/App.js
@@ -1,10 +1,12 @@
-import React, {Component} from 'react';
-import ErrorPage404 from './component/ErrorPage404';
+import React, { Component } from "react";
+import ErrorPage404 from "./component/ErrorPage404";
+import SelectHospital from "./component/SelectHospital";
 
 function App() {
   return (
     <div>
-		<ErrorPage404/>
+      {/*<ErrorPage404 />*/}
+      <SelectHospital />
     </div>
   );
 }
---
+++ HomeHospital-Frontend/hh-front/src/component/SelectHospital.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function SelectHospital() {
+  return;
+  <div>SelectHospital</div>;
+}
+
+export default SelectHospital;
