58842dfbf158c3905078870b23045328e0772df5
Ridge Banez <84418684+RidgeBnz@users.noreply.github.com>
2022-03-03 09:41:47 -0700

    Update App.js
    fixed

--- HomeHospital-Frontend/hh-front/src/App.js
+++ HomeHospital-Frontend/hh-front/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import ErrorPage404 from "./components/ErrorPage404";
-import SelectHospital from "./components/SelectHospital";
 
 function App() {
   return (
     <div>
-      {/*<ErrorPage404 />*/}
-      <SelectHospital />
+      <ErrorPage404 />
     </div>
   );
 }
