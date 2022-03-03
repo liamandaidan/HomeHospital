d9dc5b63eddd55148a6726cd86aca117ad80b10f
Brett Semmler <4926252+BSemmler@users.noreply.github.com>
2022-03-03 10:29:04 -0700

    Update user.service.js

--- HomeHospital-Backend/src/api/service/user.service.js
+++ HomeHospital-Backend/src/api/service/user.service.js
@@ -2,7 +2,7 @@ import patientModel from "../../models/patient.Model.js";
 
 export async function getPatientInfoVisitRequest(patientEmail) {
     const patientQuery = { email: patientEmail }
-    const fields = { user: { firstName: 1, lastName: 1, phoneNumber: 1 }, HCNumber: 1, 
+    const fields = { user: { firstName: 1, lastName: 1, phoneNumber: 1 }, HCnumber: 1, 
                     emergencyContact: {firstName: 1, phoneNumber: 1} }
 
     // Check if patient exists and if so retrieve it. If not return null.
