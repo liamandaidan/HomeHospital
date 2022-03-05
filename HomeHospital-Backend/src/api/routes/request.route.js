45d3fc6b46e2d603fd393b9a282e66c3b3fa336f
Michael Cann <mikepcannbass@gmail.com>
2022-03-05 11:58:41 -0700

    Updated request with Bushra, patient can create a request that will get saved into the database

--- HomeHospital-Backend/src/api/routes/request.route.js
+++ HomeHospital-Backend/src/api/routes/request.route.js
@@ -1,2 +1,67 @@
 import express from 'express'
+import patientModel from '../../models/patient.Model.js'
+import medicalFacilityModel from '../../models/medicalFacility.Model.js'
+import mongoose from 'mongoose'
+import visitRequest from '../../models/visitRequest.Model.js'
 
+const app = express.Router()
+
+app.post('/newRequest', async (req, res) => {
+	// get the HospitalID
+	// get patient ID
+	// get list of symptom's (array) and additional info
+	const { hospitalID, patientID, symptomList, additionalInfo } = req.body
+
+	// Validates that the ID's for the hospital and patient are valid Mongo IDs
+	const validFacilityID = mongoose.Types.ObjectId.isValid(hospitalID)
+	const validUserID = mongoose.Types.ObjectId.isValid(patientID)
+
+	if (validFacilityID && validUserID) {
+		// Fetch the patients address
+		const patient = await patientModel.findById(patientID)
+		const hospital = await medicalFacilityModel.findById(hospitalID)
+		const { address } = patient.user
+
+		if (patient && hospital) {
+			try {
+				// Create the new request
+				const request = await visitRequest.create({
+					patient: patient._id, //patientOID
+					requestHospitalID: hospital._id, //hospitalID,
+					// sets the patients address by default to the starting address
+					startAddress: {
+						streetAddress: address.streetAddress,
+						cityName: address.cityName,
+						provName: address.provName,
+						postalCode: address.postalCode,
+					},
+					symptoms: symptomList,
+					additionalInfo: additionalInfo,
+				})
+
+				// Save the request to the DB if all is OK
+				request.save()
+				
+				console.log(`New Patient request added to the DB: ${request._id}`)
+				res.send({ message: 'this worked', data: symptomList })
+			} catch (error) {
+				console.log(`Error: ${error.message}`)
+				res.status(400).send({ message: 'you fucked up:' })
+			}
+		} else {
+			console.log('Patient or hospital Do no exist')
+			res.status(400).send({ message: 'you fucked up:' })
+		}
+	} else {
+		console.log("Object ID's are Not valid")
+		res.status(400).send({ message: 'you fucked up' })
+	}
+
+	// create the request
+
+	//save request
+
+	//send back user confirmation that request was made along with request ID
+})
+
+export default app
--- HomeHospital-Backend/src/models/visitRequest.Model.js
+++ HomeHospital-Backend/src/models/visitRequest.Model.js
@@ -5,23 +5,28 @@ import symptomSchema from './symptom.Schema.js'
 
 const visitRequestSchema = new mongoose.Schema({
 	patient: {
-		type: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient' },
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Patient',
 		required: true,
 	},
 	requestHospitalID: {
-		type: { type: mongoose.Schema.Types.ObjectId, ref: 'MedicalFacility' },
-		default: null,
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'MedicalFacility',
+		required: true,
+	},
+	symptoms: {
+		type: [symptomSchema],
 		required: true
+	},
+	additionalInfo: {
+		type: String,
+		maxlength: 200,
+		default: null,
 	},
 	startAddress: addressSchema,
 	vitals: {
 		type: vitalsSchema,
 		default: null,
-	},
-	symptoms: [symptomSchema],
-	additionalInfo: {
-		type: String,
-		maxlength: 200,
 	},
 	dateTime: {
 		type: Date,
@@ -32,5 +37,6 @@ const visitRequestSchema = new mongoose.Schema({
 		default: false,
 	},
 })
+
 
 export default mongoose.model('VisitRequest', visitRequestSchema)
