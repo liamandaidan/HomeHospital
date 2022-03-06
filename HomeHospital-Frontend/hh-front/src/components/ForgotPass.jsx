693ad5d651967ec2f617d7238e59780e44a9b6e5
Liam McLaughlin <84680661+liamandaidan@users.noreply.github.com>
2022-03-05 20:30:48 -0700

    Fixed BG

--- HomeHospital-Frontend/hh-front/src/components/ForgotPass.jsx
+++ HomeHospital-Frontend/hh-front/src/components/ForgotPass.jsx
@@ -4,66 +4,75 @@ import "../styles/forgotpass.css";
 import logo1 from "../images/hb1.png";
 import logo2 from "../images/hb2.png";
 import { useNavigate } from "react-router-dom";
+import bg from "../images/bg.png";
 
 export default function ForgotPass() {
   let navigate = useNavigate();
   return (
-    <Container>
-      <div className="bgdiv">
-        <div className="content">
-          <div className="med" />
+    <div
+      className="bgdiv"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "repeat",
+        backgroundSize: "cover",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <div className="content">
+        <div className="med" />
 
-          <div className="leftBtm">
-            <Row>
-              <Col>{/* <p className="fp">Forgot Password?</p> */}</Col>
-            </Row>
+        <div className="leftBtm">
+          <Row>
+            <Col>{/* <p className="fp">Forgot Password?</p> */}</Col>
+          </Row>
 
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Row>
-                  <Form.Label className="fp">Forgot Password?</Form.Label>
-                </Row>
-                <Row>
-                  <Form.Control type="email" placeholder="Enter email" />
-                </Row>
-              </Form.Group>
-              <Button
-                className="col-xs-3"
-                variant="secondary"
-                type="back"
-                onClick={()=>{
-                  navigate("/login");
-                }}
-              >
-                Back
-              </Button>
-              &nbsp;
-              <Button
-                className="btnSpace"
-                variant="primary"
-                type="submit"
-                onClick={()=>{
-                  navigate("/*");
-                }}
-              >
-                Submit
-              </Button>
-            </Form>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Row>
+                <Form.Label className="fp">Forgot Password?</Form.Label>
+              </Row>
+              <Row>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Row>
+            </Form.Group>
+            <Button
+              className="col-xs-3"
+              variant="secondary"
+              type="back"
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              Back
+            </Button>
+            &nbsp;
+            <Button
+              className="btnSpace"
+              variant="primary"
+              type="submit"
+              onClick={() => {
+                navigate("/*");
+              }}
+            >
+              Submit
+            </Button>
+          </Form>
+        </div>
+
+        <div className="logo1">
+          <img src={logo1} className="logo1"></img>
+        </div>
+        <div className="right">
+          <div className="title">
+            <p>Home Hospital</p>
           </div>
-
-          <div className="logo1">
-            <img src={logo1} className="logo1"></img>
-          </div>
-          <div className="right">
-            <div className="title">
-              <p>Home Hospital</p>
-            </div>
-            <div className="logo2">
-              <img src={logo2} className="logo2"></img>
-            </div>
+          <div className="logo2">
+            <img src={logo2} className="logo2"></img>
           </div>
         </div>
       </div>
-    </Container>
+    </div>
   );
 }
