20a8077219a0e29f2312a91e91b298d6b491bdee
Benson-Davis <benson.davis@edu.sait.ca>
2022-03-07 15:51:40 -0700

    Added some stuff

--- HomeHospital-Backend/src/api/routes/passReset.route.js
+++ HomeHospital-Backend/src/api/routes/passReset.route.js
@@ -3,10 +3,11 @@ import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import ENV from '../../configure/configure.js'
 //import sendEmail from '../service/sendEmail.js'
-import sendEmailAlt from '../service/sendEmailAlt.js'
+import sendEmailAlt from '../service/sendEmail.service.js'
 import sendEmail from '../service/emailTutorial.js'
 //const sendEmail = require('../service/sendEmail.cjs');
 import handlebar from 'handlebars'
+import { updatePassword } from '../service/resetPass.service.js'
 
 
 import PatientModel from '../../models/patient.Model.js'
@@ -17,24 +18,40 @@ const route = express.Router()
 
 //route for when the user clicks to reset their password
 route.post('/', async (req, res, next) => {
-    const {email} = req.body;
+    const {email, newPass, newPassConfirm} = req.body;
     console.log("Got email: " + email);
-    try {
-        const patient = await PatientModel.findOne({ email: email })
-        console.log("Return from DB is: " + patient);
-        if(patient != null) {
-            const resettoken = jwt.sign({email: email}, resetKey, {expiresIn: "24h"});//verify should return the email
-            console.log("Token is: " + resettoken);
-            const link = `${clientURL}?uemail=${email}&tokenstring=${resettoken}`;
-            console.log("Link is: " + link);
-            console.log("Name is: " + patient.user.firstName);
-            sendEmailAlt(email, "Password Reset Request", {name: patient.user.firstName, link: link});
-            res.status(201).send({ message: "Send mail complete" })
+    if(email){
+        if(newPass && newPassConfirm) {//these parameters will only exist if user has entered a new password and confirmed
+            const updateResult = await updatePassword(email, newPass);
+            console.log("Result: " + updateResult);
+            if(updateResult === 1)
+            {
+                res.status(201).send({ message: "Password successfully updated" })
+            } else {
+                res.status(406).send({ message: "Password update failed" })
+            }
+        } else {
+            try {
+                const patient = await PatientModel.findOne({ email: email })
+                console.log("Return from DB is: " + patient);
+                if(patient != null) {
+                    const resettoken = jwt.sign({email: email}, resetKey, {expiresIn: "24h"});//verify should return the email
+                    console.log("Token is: " + resettoken);
+                    const link = `${clientURL}?uemail=${email}&tokenstring=${resettoken}`;
+                    console.log("Link is: " + link);
+                    console.log("Name is: " + patient.user.firstName);
+                    sendEmailAlt(email, "Password Reset Request", {name: patient.user.firstName, link: link});
+                    res.status(201).send({ message: "Send mail complete" })
+                }
+            } catch(e) {
+                console.error(e.message)
+                res.status(406).send('Request Failed')
+            }
         }
-    } catch(e) {
-        console.error(e.message)
-		res.status(406).send('Request Failed')
+    } else {
+        res.status(401).send('Information is required')
     }
+    
 })
 
 
@@ -46,6 +63,8 @@ route.get('/', (req, res, next) => {
     console.log("Tokenstring: " + tokenstring);
     res.status(201);
 })
+
+
 
 
 export default route
---
+++ HomeHospital-Backend/src/models/waitlist.class.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import fs from 'fs'
+
+
+class Waitlist {
+
+    
+}
