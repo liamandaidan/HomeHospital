839bce1efa2a7bd2f237921861b364bdc9d165d4
Benson-Davis <benson.davis@edu.sait.ca>
2022-03-07 18:51:25 -0700

    Waitlist class mostly done, populateWaitlist service partly done

--- HomeHospital-Backend/src/api/service/populateWaitlists.service.js
+++ HomeHospital-Backend/src/api/service/populateWaitlists.service.js
@@ -1,2 +1,21 @@
-import express from 'express'
 import fs from 'fs'
+import VisitRequest from '../../models/visitRequest.Model.js';
+
+export async function populateWaitlists() {
+    let allRequests = await VisitRequest.find();
+    if(allRequests.length === 0) {
+        fs.readFile('../HomeHospital-Backend/src/requestOrder.txt', 'utf8', (err, data) => {
+            if(err) {
+                console.error(err);
+                return;
+            }
+            console.log(data);
+        })
+    } else {
+        //console.log(allRequests);
+        allRequests.forEach(element => {
+            console.log(element);
+        })
+    }
+    
+}
--- HomeHospital-Backend/src/models/waitlist.class.js
+++ HomeHospital-Backend/src/models/waitlist.class.js
@@ -1,8 +1,102 @@
 import express from 'express'
 import fs from 'fs'
+import Request from './visitRequest.Model.js'
 
 
-class Waitlist {
+export class Waitlist {
+    #hospital;
+    #queueSize;
+    #queue = [];
+    /**
+     * 
+     * @param {JS Object} hospital, contains name and hospitalID 
+     */
+    constructor(hospital) {
+        this.#hospital = hospital;
+        this.#queueSize = 0;
+    }
 
-    
+    /**
+     * Add a Patient to the end of the queue 
+     * @param {Request} Request 
+     * @returns 
+     */
+    enqueue(Request) {
+        this.#queue.push(Request);
+        this.#queueSize++;
+        return 1;
+    }
+
+    /**
+     * Remove the first Request in the queue. Return -1 if the Request doesn't exist
+     * @param {Request} Request 
+     * @returns 1 on success, -1 if Request doesn't exist
+     */
+    dequeue(Request) {
+        if(this.#queue.includes(Request)) {
+            this.#queue = this.#queue.shift();
+            this.#queueSize--;
+            return 1;
+        } else {
+            return -1;
+        }
+        
+    }
+
+    /**
+     * Add a Request to a specific place in the queue. position refers to the place in the queue to
+     * insert, and the 0 refers to the number of elements to remove, in this case none. Inserting the
+     * new Request here will shift elements to the right of position.
+     * @param {Request} Request 
+     * @param {int} position 
+     * @returns 1 on success, -1 if the position is out of bounds of the queue
+     */
+    insert(Request, position) {
+        if(position > this.#queue.length || position < 0) {
+            return -1;
+        }
+        this.#queue.splice(position, 0, Patient);
+        this.#queueSize++;
+        return 1;
+    }
+
+    /**
+     * Remove a single Request from the queue based on Request._id
+     * @param {Request} Request 
+     * @returns 1 on success, -1 if Request doesn't exist
+     */
+    delete(Request) {
+        const index = this.#queue.indexOf(Request);
+        if(index >= 0) {
+            this.#queue.splice(index, 1);
+            this.#queueSize--;
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Remove a Request from its current position in the queue and place it in the new position by 
+     * calling this class' insert method
+     * @param {Request} Request 
+     * @param {int} newPos 
+     * @returns 1 on success, -1 if Request doesn't exist or newPos is out of bounds of the queue
+     */
+    move(Request, newPos) {
+        if(newPos > this.#queue.length || newPos < 0) {
+            return -1;
+        }
+        const theRequest = Request;
+        const index = this.#queue.indexOf(Patient);
+        if(index >= 0) {
+            this.#queue.splice(index, 1);
+            this.#queueSize--;//this is here so that if something goes wrong with re-adding the Request, the queue size remains accurate
+            this.insert(theRequest, newPos);
+            this.#queueSize++;
+            return 1;
+        } else {
+            return -1;
+        }
+    }
 }
