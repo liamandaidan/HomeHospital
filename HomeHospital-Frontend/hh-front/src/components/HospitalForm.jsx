efae1959011a9e794059717ea692512732eb1d25
RidgeBnz <ridge.banez@edu.sait.ca>
2022-03-08 08:04:30 -0700

    Hospital selection page
    Started hospital page

--- HomeHospital-Frontend/hh-front/src/components/HospitalForm.jsx
+++ HomeHospital-Frontend/hh-front/src/components/HospitalForm.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function SelectHospital() {
   return (
-	<div></div>
+	<>
+		<Container className="hospitalList-container">
+			<Row>
+				<div className="hospitalList-title">
+					<h2>Select Hospital</h2>
+				</div>
+			</Row>
+			<Row>
+				<div className="hospitalChoice">
+					<h2>Please select the hospital you would like to visit</h2>
+				</div>
+			</Row>
+			
+		
+		
+		
+		
+		</Container>
+	</>
 
   )
 }
--- HomeHospital-Frontend/hh-front/src/pages/HospitalSelectionPage.js
+++ HomeHospital-Frontend/hh-front/src/pages/HospitalSelectionPage.js
@@ -3,6 +3,7 @@ import Navigation from "../components/Navigation.js";
 import PatientInfo from "../components/PatientInfo.js";
 import BreadcrumbNav from "../components/BreadcrumbNav.js";
 import ProgressNav from "../components/ProgressNav.js";
+import HospitalForm from "../components/HospitalForm.jsx"
 import "../styles/main.css";
 
 import { Container, Row, Col } from "react-bootstrap";
@@ -28,8 +29,11 @@ function HospitalSelectionPage() {
         </Row>
         <Row>
           <Col>
-            <PatientInfo />
+		  <HospitalForm />
           </Col>
+		  <Col>
+		  <PatientInfo />
+		  </Col>
         </Row>
       </Container>
 	</>
