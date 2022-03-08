94320a543f5407c58a6719b1823f77c9c758c3a1
Lance Gee <Lancegee.apps@gmail.com>
2022-03-08 08:21:09 -0700

    grid area created

--- HomeHospital-Frontend/hh-front/src/pages/User.js
+++ HomeHospital-Frontend/hh-front/src/pages/User.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
 import UserNavBar from "../components/UserNavBar";
+import classes from "./User.module.css";
 
 function User() {
   return (
     <React.Fragment>
       <UserNavBar />
+      <Container>
+        <Row>
+          <Col>
+            <Row className={classes.boxArea}>PROFILE COMPONENT</Row>
+            <Row className={classes.boxArea}>MAP COMPONENT</Row>
+          </Col>
+          <Col>
+            <Row className={classes.boxArea}>CURRENT WAITLIST COMPONENT</Row>
+            <Row className={classes.boxArea}>SYMPTOMS COMPONENT</Row>
+            <Row className={classes.boxArea}>VITALS COMPONENT</Row>
+          </Col>
+        </Row>
+      </Container>
     </React.Fragment>
   );
 }
