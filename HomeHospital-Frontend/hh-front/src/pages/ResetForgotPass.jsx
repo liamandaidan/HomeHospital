0bfc2d325e727651820ca826e39b5d6e926f82ef
Liam McLaughlin <84680661+liamandaidan@users.noreply.github.com>
2022-03-08 16:43:05 -0700

    resetForgotPass

--- HomeHospital-Frontend/hh-front/src/pages/ResetForgotPass.jsx
+++ HomeHospital-Frontend/hh-front/src/pages/ResetForgotPass.jsx
@@ -5,21 +5,30 @@ import bg from "../images/bg.png";
 import "../styles/forgotpass.css";
 import logo1 from "../images/hb1.png";
 import logo2 from "../images/hb2.png";
+import Axios from "axios";
 export default function ResetForgotPass() {
   //create some hooks and nav
   let navigate = useNavigate();
+  let url = window.location.href;
+  let [val1, val2] = url.split("?uemail=")[1].split("&tokenstring=");
   //password var
   const [password, setPassword] = useState("");
   const [verifyPassword, setVerifyPassword] = useState("");
   //if password is valid
   const [validPassword, setValidPassword] = useState(false);
 
+  // function findInputs() {
+  //   let url = window.location.href;
+  //   let [val1, val2] = url.split("?uemail=")[1].split("&tokenstring=");
+  //   console.log(val1+" "+val2);
+  //   //alert(val1 + " " + val2);
+  // }
+
   /**
    * This function will serve to sanitize the password data, and compare the two inputs
    */
   function validatePassword() {
-      //pass must be a min length of 5
-
+    //pass must be a min length of 5
     if (password === verifyPassword && password.length > 4) {
       document.getElementById("password").classList.remove("is-invalid");
       document.getElementById("password").classList.add("is-valid");
@@ -38,9 +47,11 @@ export default function ResetForgotPass() {
    * This will submit on click. Assuming that validation has gone through.
    */
   function submitFunc() {
-    Axios.post("localhost:4000/api/forget", {
-      password: password,
-
+    Axios.post("http://localhost:4000/api/forget", {
+      email: val1,
+      token: val2,
+      newPass: password,
+      newPassConfirm: verifyPassword,
     })
       .then((response) => {
         console.log("Sent a password request through");
@@ -113,7 +124,7 @@ export default function ResetForgotPass() {
             <Button
               className="btnSpace"
               variant="primary"
-              type="submit"
+              type="button"
               disabled={!validPassword}
               onClick={submitFunc}
             >
