0ed950234958ccd2cb43c502d5a95d554da743cd
Michael Cann <mikepcannbass@gmail.com>
2022-03-10 09:34:52 -0700

    Refactor user route

--- HomeHospital-Backend/src/api/routes/users.route.js
+++ HomeHospital-Backend/src/api/routes/users.route.js
@@ -1,21 +1,26 @@
 import express from 'express'
-import { getPatientInfoVisitRequest } from "../service/user.service.js";
+import { getPatientInfoVisitRequest } from '../service/user.service.js'
 
 // Creates Router
 const route = express.Router()
 
 // Register Route
 route.post('/PatientInfoVisitRequest', async (req, res) => {
-    // Get the patient.
-   const { email } = req.body
-   const patient = await getPatientInfoVisitRequest(email)
+	// Get the patient.
+	const { email } = req.body
+	try {
+		const patient = await getPatientInfoVisitRequest(email)
 
-   // Check to see that the patient exists.
-   if (patient == null) {
-       res.status(406).send({status: 'Error', message: 'Cannot find patient'})
-   } else {
-       res.send(patient)
-   }
+		// Check to see that the patient exists.
+		if (patient == null) {
+			res.status(406).send({
+				status: 'Error',
+				message: 'Cannot find patient',
+			})
+		} else {
+			res.status(200).send({data: patient})
+		}
+	} catch (error) {}
 })
 
 export default route
