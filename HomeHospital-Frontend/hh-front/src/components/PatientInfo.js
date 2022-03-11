f352d415c318717846f9fa9c68bf69400b59df1f
rawbz <rmjbalanag@gmail.com>
2022-03-10 19:48:32 -0700

    updated PatietnInfo
    able to display patient info from endpoint

--- HomeHospital-Frontend/hh-front/src/components/PatientInfo.js
+++ HomeHospital-Frontend/hh-front/src/components/PatientInfo.js
@@ -13,15 +13,27 @@ import axios from 'axios';
 function PatientInfo() {
 
   const [email, setEmail] = useState("robynbalanag@gmail.com");
-  const [patient, setPatient] = useState({});
-
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [HCNumber, setHCNumber] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [emergFirstName, setEmergFirstName] = useState("");
+  const [emergLastName, setEmergLastName] = useState("");
+  const [emergPhoneNumber, setEmergPhoneNumber] = useState("");
+  
 
 
   useEffect(() => {
     axios.post('http://localhost:4000/api/users/PatientInfoVisitRequest', {
       email: email,
     }).then((response) => {
-      console.log(response.data);
+      setFirstName(response.data.data.user.firstName);
+      setLastName(response.data.data.user.lastName);
+      setPhoneNumber(response.data.data.user.phoneNumber);
+      setHCNumber(response.data.data.HCnumber);
+      setEmergFirstName(response.data.data.emergencyContact.firstName);
+      setEmergLastName(response.data.data.emergencyContact.lastName);
+      setEmergPhoneNumber(response.data.data.emergencyContact.phoneNumber);
     }).catch((err) => {
       console.log(err);
     })
@@ -37,14 +49,18 @@ function PatientInfo() {
         </Row>
         <Row>
           <Col>
+          </Col>
+          <Col>
             <img src={profile} alt="profilePic" className="profilepic" />
           </Col>
           <Col md={8}>
             <div className="patient-info-requestDetails">
-              <h3>Linda Belcher</h3>
+              <h3>{firstName} {lastName}</h3>
               <p>Visit request #45</p>
               <p>Scheduled for March 15, 2022</p>
             </div>
+          </Col>
+          <Col>
           </Col>
         </Row>
         <Row>
@@ -56,22 +72,17 @@ function PatientInfo() {
         </Row>
         <Row>
           <Col className="patient-contactDetails">
-            <p>Contact number: (403) 123-1234</p>
-            <p>Alberta Health Care no: 123456-12</p>
-            <p>Emergency contact name: Bob Belcher</p>
-            <p>Contact contanct number: (403) 222-1111</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div className="edit">
-            <a href="">Edit details</a>
-            </div>
+            <p>Contact number: {phoneNumber}</p>
+            <p>Alberta Health Care no: {HCNumber}</p>
+            <p>Emergency contact name: {emergFirstName} {emergLastName}</p>
+            <p>Contact contanct number: {emergPhoneNumber}</p>
           </Col>
         </Row>
         <Row>
             <Col>
+            <div className="hbar-div">
             <img src={hbar} alt="heartbeat bar" className="hb-bar"/>
+            </div>
             </Col>
         </Row>
       </Container>
