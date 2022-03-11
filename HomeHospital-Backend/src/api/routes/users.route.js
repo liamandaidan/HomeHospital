aba2aa10492b8eae19871ae2ffb3b50ba928eff4
Mike Cann <mikepcannbass@gmail.com>
2022-03-10 20:43:18 -0700

    Revert "Caught bug in JWT verification with ref/access tokens"

--- HomeHospital-Backend/src/api/routes/users.route.js
+++ HomeHospital-Backend/src/api/routes/users.route.js
@@ -4,25 +4,19 @@ import patientModel from '../../models/patient.Model.js';
 // Creates Router
 const route = express.Router()
 
-
-// Register Route
-route.post('/PatientInfoVisitRequest', async (req, res) => {
-	// Get the patient.
-	const { email } = req.body
-	try {
-		const patient = await getPatientInfoVisitRequest(email)
-
-		// Check to see that the patient exists.
-		if (patient == null) {
-			res.status(406).send({
-				status: 'Error',
-				message: 'Cannot find patient',
-			})
-		} else {
-			res.status(200).send({ data: patient })
-		}
-	} catch (error) {}
-
+route.post('/PatientInfoVisitRequest', async ( req, res ) => {
+   // Check to see that the patient exists. If so send the appropriate info, if not send error code.
+   try {
+      if ( await patientModel.exists( req.body ) ) {
+         const patient = await patientModel.findOne( req.body )
+         const patientOut = patient.getPatientRequestInfo()
+         res.send( { data: patientOut });
+      } else {
+         res.status(406).send({status: 'Error', message: 'Cannot find patient'})
+      }
+   } catch (error) {
+      console.error(error.message)
+   }
 })
 
 export default route
