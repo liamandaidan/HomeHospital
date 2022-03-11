e20a96ae7ac87bbfea7b7f87e3cd43be9715f8d7
Michael Cann <mikepcannbass@gmail.com>
2022-03-10 20:57:12 -0700

    Fixed merge issue with the user route

--- HomeHospital-Backend/src/api/routes/users.route.js
+++ HomeHospital-Backend/src/api/routes/users.route.js
@@ -7,22 +7,21 @@ const route = express.Router()
 
 // Register Route
 route.post('/PatientInfoVisitRequest', async (req, res) => {
-	// Get the patient.
-	const { email } = req.body
+	// Check to see that the patient exists. If so send the appropriate info, if not send error code.
 	try {
-		const patient = await getPatientInfoVisitRequest(email)
-
-		// Check to see that the patient exists.
-		if (patient == null) {
+		if (await patientModel.exists(req.body)) {
+			const patient = await patientModel.findOne(req.body)
+			const patientOut = patient.getPatientRequestInfo()
+			res.send({ data: patientOut })
+		} else {
 			res.status(406).send({
 				status: 'Error',
 				message: 'Cannot find patient',
 			})
-		} else {
-			res.status(200).send({ data: patient })
 		}
-	} catch (error) {}
-
+	} catch (error) {
+		console.error(error.message)
+	}
 })
 
 export default route
