98ffb4bc61e5c73b07431c2cc7c5c250378a3296
Liam McLaughlin <84680661+liamandaidan@users.noreply.github.com>
2022-03-17 08:43:15 -0600

    PractionerW8List

--- HomeHospital-Frontend/hh-front/src/components/PractionerWaitlist.jsx
+++ HomeHospital-Frontend/hh-front/src/components/PractionerWaitlist.jsx
@@ -1,6 +1,17 @@
-import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import React, { useContext, useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import { Axios } from "axios";
 export default function PractionerWaitlist() {
+  const [posts, setPosts] = useState("");
+
+//   useEffect(() => {
+//     //to be defined later!
+//     Axios.get("http://localhost:4000/................").then((response) => {
+//       console.log(response.data);
+//       setPosts(response.data);   
+//     });
+//   }, []);
+
   return (
     <div>
       {/* This table class will be automatically generated.
@@ -11,6 +22,7 @@ Just used as an example for now */}
             <th scope="col">#</th>
             <th scope="col">First</th>
             <th scope="col">Last</th>
+            <th scope="col">Details</th>
             <th scope="col"></th>
           </tr>
         </thead>
@@ -20,20 +32,29 @@ Just used as an example for now */}
             <td>Mark</td>
             <td>Otto</td>
             <td>
+              <Button>Show</Button>
+            </td>
+            <td>
               <Button>Check in</Button>
             </td>
           </tr>
           <tr>
             <th scope="row">2</th>
             <td>Jacob</td>
-            <td>Thornton</td>
+            <td>Thornton</td>{" "}
+            <td>
+              <Button>Show</Button>
+            </td>
             <td>
               <Button>Check in</Button>
             </td>
           </tr>
           <tr>
             <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
+            <td colspan="2">Larry the Bird</td>{" "}
+            <td>
+              <Button>Show</Button>
+            </td>
             <td>
               <Button>Check in</Button>
             </td>
