2f152d409af2a65045be4925e6cd041356e78d74
Liam McLaughlin <84680661+liamandaidan@users.noreply.github.com>
2022-03-17 10:31:45 -0600

    Changed function to classes

--- HomeHospital-Frontend/hh-front/src/components/PractionerWaitlist.jsx
+++ HomeHospital-Frontend/hh-front/src/components/PractionerWaitlist.jsx
@@ -1,66 +1,125 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, Component } from "react";
 import { Button } from "react-bootstrap";
 import { Axios } from "axios";
-export default function PractionerWaitlist() {
-  const [posts, setPosts] = useState("");
 
-//   useEffect(() => {
-//     //to be defined later!
-//     Axios.get("http://localhost:4000/................").then((response) => {
-//       console.log(response.data);
-//       setPosts(response.data);   
-//     });
-//   }, []);
+export default class PractionerWaitlist extends Component {
 
-  return (
-    <div>
-      {/* This table class will be automatically generated.
-Just used as an example for now */}
-      <table class="table table-hover">
-        <thead class="table-light">
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Details</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>
-              <Button>Show</Button>
-            </td>
-            <td>
-              <Button>Check in</Button>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>{" "}
-            <td>
-              <Button>Show</Button>
-            </td>
-            <td>
-              <Button>Check in</Button>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>{" "}
-            <td>
-              <Button>Show</Button>
-            </td>
-            <td>
-              <Button>Check in</Button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+    render() {
+    return (
+        <div>
+          {/* This table class will be automatically generated.
+    Just used as an example for now */}
+          <table class="table table-hover">
+            <thead class="table-light">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Details</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>
+                  <Button>Show</Button>
+                </td>
+                <td>
+                  <Button>Check in</Button>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>{" "}
+                <td>
+                  <Button>Show</Button>
+                </td>
+                <td>
+                  <Button>Check in</Button>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>{" "}
+                <td>
+                  <Button>Show</Button>
+                </td>
+                <td>
+                  <Button>Check in</Button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      );
+  }
 }
+
+
+// export default function PractionerWaitlist() {
+//   const [posts, setPosts] = useState("");
+
+// //   useEffect(() => {
+// //     //to be defined later!
+// //     Axios.get("http://localhost:4000/................").then((response) => {
+// //       console.log(response.data);
+// //       setPosts(response.data);   
+// //     });
+// //   }, []);
+
+//   return (
+//     <div>
+//       {/* This table class will be automatically generated.
+// Just used as an example for now */}
+//       <table class="table table-hover">
+//         <thead class="table-light">
+//           <tr>
+//             <th scope="col">#</th>
+//             <th scope="col">First</th>
+//             <th scope="col">Last</th>
+//             <th scope="col">Details</th>
+//             <th scope="col"></th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           <tr>
+//             <th scope="row">1</th>
+//             <td>Mark</td>
+//             <td>Otto</td>
+//             <td>
+//               <Button>Show</Button>
+//             </td>
+//             <td>
+//               <Button>Check in</Button>
+//             </td>
+//           </tr>
+//           <tr>
+//             <th scope="row">2</th>
+//             <td>Jacob</td>
+//             <td>Thornton</td>{" "}
+//             <td>
+//               <Button>Show</Button>
+//             </td>
+//             <td>
+//               <Button>Check in</Button>
+//             </td>
+//           </tr>
+//           <tr>
+//             <th scope="row">3</th>
+//             <td colspan="2">Larry the Bird</td>{" "}
+//             <td>
+//               <Button>Show</Button>
+//             </td>
+//             <td>
+//               <Button>Check in</Button>
+//             </td>
+//           </tr>
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// }
--- HomeHospital-Frontend/hh-front/src/pages/Practioner.jsx
+++ HomeHospital-Frontend/hh-front/src/pages/Practioner.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import PatientInfo from "../components/PatientInfo.js";
 import PractNav from "../components/PractNavBar.jsx";
@@ -6,54 +6,53 @@ import logo from "../images/heartbeat.png";
 import "../styles/PractionerStyles.css";
 import line from "../images/hb2.png";
 import PractionerWaitlist from "../components/PractionerWaitlist.jsx";
-import PractitionerPatientInfo from "../components/PractitionerPatientInfo.js"
+import PractitionerPatientInfo from "../components/PractitionerPatientInfo.js";
 
-export default function Practioner() {
-  // change the state to view visability on patient details
-  const [patientDetails, setPatientDetails] = useState(true);
-
-  return (
-    <div>
-      <PractNav />
-      <Container className="main-container">
-        <div className="practionerView-div">
-          <Row>
-            <Col md="5">
-              <div className="leftContent" hidden={!patientDetails}>
-                <p class="pd">Patient Details</p>
-                <Row>
-                  <img
-                    src={line}
-                    class="rounded float-start"
-                    alt="RIP"
-                    className="hb2"
-                  />
-                </Row>
-                <Row>
-                  {/* Patient info shown depending on logic of component selected.
-                  PATIENTINFO COMPONENT WILL NEED TO BE REPLACED*/}
-                  <PractitionerPatientInfo />
-                </Row>
-                <Row>
-                  <p>
-                    <a href="/editPatient">Edit patient file</a>
-                  </p>
-                </Row>
-              </div>
-            </Col>
-            <Col md="7">
-              <div className="rightContent">
-                <Row>
-                  <p class="pd">Current Waitlist</p>
-                </Row>
-                <Row>
-                  <PractionerWaitlist />
-                </Row>
-              </div>
-            </Col>
-          </Row>
-        </div>
-      </Container>
-    </div>
-  );
+export default class Practioner extends Component {
+  render() {
+    return (
+      <div>
+        <PractNav />
+        <Container className="main-container">
+          <div className="practionerView-div">
+            <Row>
+              <Col md="5">
+                <div className="leftContent">
+                  <p class="pd">Patient Details</p>
+                  <Row>
+                    <img
+                      src={line}
+                      class="rounded float-start"
+                      alt="RIP"
+                      className="hb2"
+                    />
+                  </Row>
+                  <Row>
+                    {/* Patient info shown depending on logic of component selected.
+                    PATIENTINFO COMPONENT WILL NEED TO BE REPLACED*/}
+                    <PractitionerPatientInfo />
+                  </Row>
+                  <Row>
+                    <p>
+                      <a href="/editPatient">Edit patient file</a>
+                    </p>
+                  </Row>
+                </div>
+              </Col>
+              <Col md="7">
+                <div className="rightContent">
+                  <Row>
+                    <p class="pd">Current Waitlist</p>
+                  </Row>
+                  <Row>
+                    <PractionerWaitlist />
+                  </Row>
+                </div>
+              </Col>
+            </Row>
+          </div>
+        </Container>
+      </div>
+    );
+  }
 }
