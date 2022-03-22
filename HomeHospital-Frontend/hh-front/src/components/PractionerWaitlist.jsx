e2eb262ea114191945f2284936733367aa2efcd0
Liam McLaughlin <84680661+liamandaidan@users.noreply.github.com>
2022-03-21 21:17:25 -0600

    Functionality Complete

--- HomeHospital-Frontend/hh-front/src/components/PractionerWaitlist.jsx
+++ HomeHospital-Frontend/hh-front/src/components/PractionerWaitlist.jsx
@@ -3,6 +3,9 @@ import { Button } from "react-bootstrap";
 import PatientData from "../components/patientData.json";
 
 export default function PractionerWaitlist({ childToParent }) {
+  /**
+   * This will be used to render table rows based off of a dummy json file i created
+   */
   const DisplayTableRows = PatientData.map((data) => {
     return (
       <tr>
@@ -10,7 +13,7 @@ export default function PractionerWaitlist({ childToParent }) {
         <td>{data.first}</td>
         <td>{data.last}</td>
         <td>
-          <Button>Select</Button>
+          <Button value={data.id} onClick={(e) => childToParent(e.target.value)}>Select</Button>
         </td>
         <td>
           <Button>Check In</Button>
@@ -36,67 +39,3 @@ export default function PractionerWaitlist({ childToParent }) {
     </div>
   );
 }
-
-// export default function PractionerWaitlist() {
-//   const [posts, setPosts] = useState("");
-
-// //   useEffect(() => {
-// //     //to be defined later!
-// //     Axios.get("http://localhost:4000/................").then((response) => {
-// //       console.log(response.data);
-// //       setPosts(response.data);
-// //     });
-// //   }, []);
-
-//   return (
-//     <div>
-//       {/* This table class will be automatically generated.
-// Just used as an example for now */}
-//       <table class="table table-hover">
-//         <thead class="table-light">
-//           <tr>
-//             <th scope="col">#</th>
-//             <th scope="col">First</th>
-//             <th scope="col">Last</th>
-//             <th scope="col">Details</th>
-//             <th scope="col"></th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           <tr>
-//             <th scope="row">1</th>
-//             <td>Mark</td>
-//             <td>Otto</td>
-//             <td>
-//               <Button>Show</Button>
-//             </td>
-//             <td>
-//               <Button>Check in</Button>
-//             </td>
-//           </tr>
-//           <tr>
-//             <th scope="row">2</th>
-//             <td>Jacob</td>
-//             <td>Thornton</td>{" "}
-//             <td>
-//               <Button>Show</Button>
-//             </td>
-//             <td>
-//               <Button>Check in</Button>
-//             </td>
-//           </tr>
-//           <tr>
-//             <th scope="row">3</th>
-//             <td colspan="2">Larry the Bird</td>{" "}
-//             <td>
-//               <Button>Show</Button>
-//             </td>
-//             <td>
-//               <Button>Check in</Button>
-//             </td>
-//           </tr>
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
--- HomeHospital-Frontend/hh-front/src/components/PractitionerPatientInfo.js
+++ HomeHospital-Frontend/hh-front/src/components/PractitionerPatientInfo.js
@@ -10,116 +10,126 @@ import "../styles/PractionerStyles.css";
 import axios from "axios";
 import { HomeHospitalContext } from "./HomeHospitalContext";
 
-
 export class PractitionerPatientInfo extends Component {
   render() {
-	  //   const { patient_id } = useContext(HomeHospitalContext);
-//   const [patientID] = patient_id;
+    //   const { patient_id } = useContext(HomeHospitalContext);
+    //   const [patientID] = patient_id;
 
-//   const [firstName, setFirstName] = useState("");
-//   const [lastName, setLastName] = useState("");
-//   const [HCNumber, setHCNumber] = useState("");
-//   const [phoneNumber, setPhoneNumber] = useState("");
-//   const [emergFirstName, setEmergFirstName] = useState("");
-//   const [emergLastName, setEmergLastName] = useState("");
-//   const [emergPhoneNumber, setEmergPhoneNumber] = useState("");
+    //   const [firstName, setFirstName] = useState("");
+    //   const [lastName, setLastName] = useState("");
+    //   const [HCNumber, setHCNumber] = useState("");
+    //   const [phoneNumber, setPhoneNumber] = useState("");
+    //   const [emergFirstName, setEmergFirstName] = useState("");
+    //   const [emergLastName, setEmergLastName] = useState("");
+    //   const [emergPhoneNumber, setEmergPhoneNumber] = useState("");
 
-//   useEffect(() => {
-//     axios
-//       .post("http://localhost:4000/api/users/PatientInfoVisitRequest", {
-//         patientId: patientID,
-//       })
-//       .then((response) => {
-//         setFirstName(response.data.data.user.firstName);
-//         setLastName(response.data.data.user.lastName);
-//         setPhoneNumber(response.data.data.user.phoneNumber);
-//         setHCNumber(response.data.data.HCnumber);
-//         setEmergFirstName(response.data.data.emergencyContact.firstName);
-//         setEmergLastName(response.data.data.emergencyContact.lastName);
-//         setEmergPhoneNumber(response.data.data.emergencyContact.phoneNumber);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   }, []);
+    //   useEffect(() => {
+    //     axios
+    //       .post("http://localhost:4000/api/users/PatientInfoVisitRequest", {
+    //         patientId: patientID,
+    //       })
+    //       .then((response) => {
+    //         setFirstName(response.data.data.user.firstName);
+    //         setLastName(response.data.data.user.lastName);
+    //         setPhoneNumber(response.data.data.user.phoneNumber);
+    //         setHCNumber(response.data.data.HCnumber);
+    //         setEmergFirstName(response.data.data.emergencyContact.firstName);
+    //         setEmergLastName(response.data.data.emergencyContact.lastName);
+    //         setEmergPhoneNumber(response.data.data.emergencyContact.phoneNumber);
+    //       })
+    //       .catch((err) => {
+    //         console.log(err);
+    //       });
+    //   }, []);
 
-//   const timeElapsed = Date.now();
-//   const today = new Date(timeElapsed);
+    //   const timeElapsed = Date.now();
+    //   const today = new Date(timeElapsed);
 
-//   const formatDate = today.toDateString();
-return (
-    <>
-      <Container className="patient-container">
-        <Row>
-          {/* <Col></Col> */}
-		  <Row>
-          <Col>
-             <div className="hbar-div">
-              <img src={hbar} alt="heartbeat bar" className="hb-bar-practitioner1" />
-            </div>
-          </Col>
-        </Row>
-          <Col>
-            <img src={profile} alt="profilePic" className="profilepic" />
-          </Col>
-          <Col md={8}>
-            <div className="practitioner-patientRequestDetails">
-              <h3>
-				  Kanye West
-                {/* {firstName} {lastName} */}
-              </h3>
-              {/* <p>Visit request #45</p> */}
-              {/* <p>{formatDate}</p> */}
-            </div>
-          </Col>
-          <Col></Col>
-        </Row>
-        <Row>
-          <Col>
-            <div className="practitioner-patientDetails">
-				<h4>Patient Details</h4>
-              {/* <h4>Confirmed Patient Details </h4> */}
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="practitioner-patientContactDetails ">
-			  <p>Address: 123 MyHouse</p>
-			  <p>Contact Number: (403) 223-2311</p>
-			  <p>Emergency Contact Name: Pete Davidson</p>
-			  <p>Emergency Contact No: (403) 443-2312</p>
-			  <p>Alberta Health Care no: 12345-0000</p>
-			  <h5>Symptoms</h5>
-			  <ul>
-				  <li>Cough (Severity: 3)</li>
-				  <li>Runny nose (Severity: 2)</li>
-				  <li>Shallow Breathing (Severity: 5)</li>
-				  <li>Sore Throat (Severity: 2)</li>
-				  <li>Headache (Severity: 5)</li>
-			  </ul>
-			  <h5>Additional Info</h5>
-			  <p>Help me I am sick *cough</p>
-			  <h5>Place in queue: 1</h5>
-            {/* <p>Contact number: {phoneNumber}</p>
+    //   const formatDate = today.toDateString();
+    return (
+      <>
+        <Container className="patient-container">
+          <Row>
+            {/* <Col></Col> */}
+            <Row>
+              <Col>
+                <div className="hbar-div">
+                  <img
+                    src={hbar}
+                    alt="heartbeat bar"
+                    className="hb-bar-practitioner1"
+                  />
+                </div>
+              </Col>
+            </Row>
+            <Col>
+              <img src={profile} alt="profilePic" className="profilepic" />
+            </Col>
+            <Col md={8}>
+              <div className="practitioner-patientRequestDetails">
+                <h3>
+                  Kanye West
+                  {/* {firstName} {lastName} */}
+                </h3>
+                {/* <p>Visit request #45</p> */}
+                {/* <p>{formatDate}</p> */}
+              </div>
+            </Col>
+            <Col></Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="practitioner-patientDetails">
+                <h4>Patient Details</h4>
+                <div class="alert alert-primary" role="alert">
+                  #This is temporary, waitlist ID: {this.props.patientDataGiven}
+                </div>
+
+                {/* <h4>Confirmed Patient Details </h4> */}
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="practitioner-patientContactDetails ">
+              <p>Address: 123 MyHouse</p>
+              <p>Contact Number: (403) 223-2311</p>
+              <p>Emergency Contact Name: Pete Davidson</p>
+              <p>Emergency Contact No: (403) 443-2312</p>
+              <p>Alberta Health Care no: 12345-0000</p>
+              <h5>Symptoms</h5>
+              <ul>
+                <li>Cough (Severity: 3)</li>
+                <li>Runny nose (Severity: 2)</li>
+                <li>Shallow Breathing (Severity: 5)</li>
+                <li>Sore Throat (Severity: 2)</li>
+                <li>Headache (Severity: 5)</li>
+              </ul>
+              <h5>Additional Info</h5>
+              <p>Help me I am sick *cough</p>
+              <h5>Place in queue: 1</h5>
+              {/* <p>Contact number: {phoneNumber}</p>
             <p>Alberta Health Care no: {HCNumber}</p> */}
-            {/* <p>
+              {/* <p>
               Emergency contact name: {emergFirstName} {emergLastName}
             </p>
             <p>Contact contanct number: {emergPhoneNumber}</p> */}
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-             <div className="hbar-div">
-              <img src={hbar} alt="heartbeat bar" className="hb-bar-practitioner2" />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="hbar-div">
+                <img
+                  src={hbar}
+                  alt="heartbeat bar"
+                  className="hb-bar-practitioner2"
+                />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
   }
 }
 
 export default PractitionerPatientInfo;
-
