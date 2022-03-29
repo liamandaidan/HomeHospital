d2f964a7711b4bae16710bd388392bf815bbc130
rawbz <rmjbalanag@gmail.com>
2022-03-29 14:35:13 -0600

    added Create User

--- HomeHospital-Frontend/hh-front/src/components/ManageUser.js
+++ HomeHospital-Frontend/hh-front/src/components/ManageUser.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import { Table, Modal, Button, Form } from "react-bootstrap";
 import Users from "../data/users.json";
 import { AdminContext } from "./AdminContext";
+import axios from "axios";
 
 function ManageUser() {
   const [modalState, setModalState] = useState(false);
@@ -17,15 +18,37 @@ function ManageUser() {
   //selected user details to edit
   const [id, setId] = useState("");
   const [type, setType] = useState("");
+  const [gender, setGender] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [DOB, setDOB] = useState("");
+  const [email, setEmail] = useState("");
   const [phoneNum, setPhoneNum] = useState("");
   const [address, setAddress] = useState("");
   const [city, setCity] = useState("");
+  const [prov, setProv] = useState("");
   const [postalCode, setPostalCode] = useState("");
   const [aHCNum, setAHCNum] = useState("");
   const [emergName, setEmergName] = useState("");
   const [emergNum, setEmergNum] = useState("");
+  const [role, setRole] = useState("");
+
+  //information for new user
+  const [new_id, setNewId] = useState("");
+  const [new_type, setNewType] = useState("");
+  const [new_gender, setNewGender] = useState("");
+  const [new_firstName, setNewFirstName] = useState("");
+  const [new_lastName, setNewLastName] = useState("");
+  const [new_DOB, setNewDOB] = useState("");
+  const [new_email, setNewEmail] = useState("");
+  const [new_phoneNum, setNewPhoneNum] = useState("");
+  const [new_address, setNewAddress] = useState("");
+  const [new_city, setNewCity] = useState("");
+  const [new_prov, setNewProv] = useState("");
+  const [new_postalCode, setNewPostalCode] = useState("");
+  const [new_aHCNum, setNewAHCNum] = useState("");
+  const [new_emergName, setNewEmergName] = useState("");
+  const [new_emergNum, setNewEmergNum] = useState("");
 
   console.log(Users);
 
@@ -120,7 +143,6 @@ function ManageUser() {
   //display the form if the admin select's edit beside the user name
   const selectEdit = (e) => {
     setUserDisplay(false);
-    // setDisplayUsers(false);
     setEditDisplay(true);
     setCreateDisplay(false);
 
@@ -131,14 +153,35 @@ function ManageUser() {
           setType(user.type);
           setFirstName(user.firstName);
           setLastName(user.lastName);
+          setGender(user.gender);
+          setDOB(user.dateOfBirth);
+          setEmail(user.email);
           setPhoneNum(user.phoneNumber);
           setAddress(user.address);
           setCity(user.city);
+          setProv(user.provName);
           setPostalCode(user.postalCode);
           setAHCNum(user.aHCNumber);
           setEmergName(user.emergencyContactName);
           setEmergNum(user.emergencyConactNumber);
         }
+        // else {
+        //   setId(user.id);
+        //   setType(user.type);
+        //   setRole(user.role);
+        //   setFirstName(user.firstName);
+        //   setLastName(user.lastName);
+        //   setDOB(user.dateOfBirth);
+        //   setEmail(user.email);
+        //   setPhoneNum(user.phoneNumber);
+        //   setAddress(user.address);
+        //   setCity(user.city);
+        //   setProv(user.prov);
+        //   setPostalCode(user.postalCode);
+        //   setAHCNum(user.aHCNumber);
+        //   setEmergName(user.emergencyContactName);
+        //   setEmergNum(user.emergencyConactNumber);
+        // }
       });
     }
   };
@@ -152,13 +195,42 @@ function ManageUser() {
 
   const closeWindow = () => {
     setMenuChoice("");
-  }
+  };
 
   const showCreate = () => {
     setCreateDisplay(true);
     setUserDisplay(false);
     setEditDisplay(false);
-  }
+  };
+
+  const createUser = () => {
+    alert("We created a new user!");
+
+      axios
+        .post("http://localhost:4000/api/users/PatientInfoVisitRequest", {
+          new_id,
+          new_type,
+          new_gender,
+          new_firstName,
+          new_DOB,
+          new_email,
+          new_phoneNum,
+          new_address,
+          new_city,
+          new_prov,
+          new_postalCode,
+          new_aHCNum,
+          new_emergName,
+          new_emergNum,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    
+  };
 
   return (
     <>
@@ -166,12 +238,14 @@ function ManageUser() {
         <div className="admin-main-div">
           <div className="header-div">
             <div className="header-1">
-            <h2>Manage Users</h2>
+              <h2>Manage Users</h2>
             </div>
             <div className="header-2">
-            <Button className="createNewUser-btn" onClick={showCreate}>Create new user</Button>
+              <Button className="createNewUser-btn" onClick={showCreate}>
+                Create new user
+              </Button>
             </div>
-            </div>
+          </div>
           <div className="manageuser-inner-div">
             {editDisplay && (
               <div className="editUser-div">
@@ -179,11 +253,39 @@ function ManageUser() {
                 <Form>
                   <Form.Group>
                     <Form.Label>User type: </Form.Label>
-                    <Form.Control
+                    <Form.Select
                       value={type}
                       onChange={(e) => setType(e.target.value)}
                       size="sm"
-                    />
+                    >
+                      {" "}
+                      <option defaultValue>{type}</option>
+                      {type === "Patient" ? (
+                        <>
+                          <option>Practitioner</option>
+                          <option>Admin</option>
+                        </>
+                      ) : (
+                        <>
+                          <option>Patient</option>
+                          <option>Admin</option>
+                        </>
+                      )}
+                    </Form.Select>
+                    <Form.Label>Gender: </Form.Label>
+                    <Form.Select
+                      value={gender}
+                      onChange={(e) => setGender(e.target.value)}
+                      size="sm"
+                    >
+                      {" "}
+                      <option defaultValue>{gender}</option>
+                      {type === "Male" ? (
+                        <option>Female</option>
+                      ) : (
+                        <option>Male</option>
+                      )}
+                    </Form.Select>
                     <Form.Label>First name: </Form.Label>
                     <Form.Control
                       value={firstName}
@@ -194,6 +296,19 @@ function ManageUser() {
                     <Form.Control
                       value={lastName}
                       onChange={(e) => setLastName(e.target.value)}
+                      size="sm"
+                    />
+                    <Form.Label>Date of birth: </Form.Label>
+                    <Form.Control
+                      type="date"
+                      value={DOB}
+                      onChange={(e) => setDOB(e.target.value)}
+                      size="sm"
+                    />
+                    <Form.Label>Email: </Form.Label>
+                    <Form.Control
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
                       size="sm"
                     />
                     <Form.Label>Phone numer: </Form.Label>
@@ -212,6 +327,12 @@ function ManageUser() {
                     <Form.Control
                       value={city}
                       onChange={(e) => setCity(e.target.value)}
+                      size="sm"
+                    />
+                    <Form.Label>Prov: </Form.Label>
+                    <Form.Control
+                      value={prov}
+                      onChange={(e) => setProv(e.target.value)}
                       size="sm"
                     />
                     <Form.Label>Postal code: </Form.Label>
@@ -249,7 +370,10 @@ function ManageUser() {
                       </a>
                     </div>
                     <div className="confirmChange-div item-2">
-                      <Button className="confirmChange-btn">
+                      <Button
+                        onClick={createUser}
+                        className="confirmChange-btn"
+                      >
                         Confirm Change
                       </Button>
                       <br />
@@ -263,7 +387,9 @@ function ManageUser() {
             )}
             {userDisplay && (
               <div className="userDisplay-div">
-                <div className="closeWindow"><a onClick={closeWindow}>close</a></div>
+                <div className="closeWindow">
+                  <a onClick={closeWindow}>close</a>
+                </div>
                 <Table className="userDisplay-table">
                   <thead>
                     <tr>
@@ -305,84 +431,101 @@ function ManageUser() {
               </div>
             )}
             {createDisplay && (
-               <div className="createUser-div">
-               {/* <h3>User ID: {id} </h3> */}
-               <Form>
-                 <Form.Group>
-                   <Form.Label>User type: </Form.Label>
-                   <Form.Control
-                     value={type}
-                     onChange={(e) => setType(e.target.value)}
-                     size="sm"
-                   />
-                   <Form.Label>First name: </Form.Label>
-                   <Form.Control
-                     value={firstName}
-                     onChange={(e) => setFirstName(e.target.value)}
-                     size="sm"
-                   />
-                   <Form.Label>Last name: </Form.Label>
-                   <Form.Control
-                     value={lastName}
-                     onChange={(e) => setLastName(e.target.value)}
-                     size="sm"
-                   />
-                   <Form.Label>Phone numer: </Form.Label>
-                   <Form.Control
-                     value={phoneNum}
-                     onChange={(e) => setPhoneNum(e.target.value)}
-                     size="sm"
-                   />
-                   <Form.Label>Address: </Form.Label>
-                   <Form.Control
-                     value={address}
-                     onChange={(e) => setAddress(e.target.value)}
-                     size="sm"
-                   />
-                   <Form.Label>City: </Form.Label>
-                   <Form.Control
-                     value={city}
-                     onChange={(e) => setCity(e.target.value)}
-                     size="sm"
-                   />
-                   <Form.Label>Postal code: </Form.Label>
-                   <Form.Control
-                     value={postalCode}
-                     onChange={(e) => setPostalCode(e.target.value)}
-                     size="sm"
-                   />
-                   <Form.Label>Alberta Health Care: </Form.Label>
-                   <Form.Control
-                     value={aHCNum}
-                     onChange={(e) => setAHCNum(e.target.value)}
-                     size="sm"
-                   />
-                   <Form.Label>Emergecy contact name: </Form.Label>
-                   <Form.Control
-                     value={emergName}
-                     onChange={(e) => setEmergName(e.target.value)}
-                     size="sm"
-                   />
-                   <Form.Label>Emergency contact number: </Form.Label>
-                   <Form.Control
-                     value={emergNum}
-                     onChange={(e) => setEmergNum(e.target.value)}
-                     size="sm"
-                   />
-                 </Form.Group>
-                 <div className="grid-div">
-                   <div className="confirmChange-div item-2">
-                     <Button className="confirmChange-btn">
-                       Confirm new user
-                     </Button>
-                     <br />
-                     <a className="admin-link" onClick={showUsers}>
-                       Cancel
-                     </a>
-                   </div>
-                 </div>
-               </Form>
-             </div>
+              <div className="createUser-div">
+                {/* <h3>User ID: {id} </h3> */}
+                <Form>
+                  <Form.Group>
+                    <Form.Label>User type: </Form.Label>
+                    <Form.Select
+                      value={type}
+                      onChange={(e) => setNewType(e.target.value)}
+                      size="sm"
+                    >
+                      <option>Patient</option>
+                      <option>Practitioner</option>
+                      <option>Admin</option>
+                    </Form.Select>
+                    <Form.Label>Gender: </Form.Label>
+                    <Form.Select
+                      onChange={(e) => setNewGender(e.target.value)}
+                      size="sm"
+                    >
+                      <option>Female</option>
+                      <option>Male</option>
+                    </Form.Select>
+                    <Form.Label>First name: </Form.Label>
+                    <Form.Control
+                      onChange={(e) => setNewFirstName(e.target.value)}
+                      size="sm"
+                    />
+                    <Form.Label>Last name: </Form.Label>
+                    <Form.Control
+                      onChange={(e) => setNewLastName(e.target.value)}
+                      size="sm"
+                    />
+                    <Form.Label>Date of birth: </Form.Label>
+                    <Form.Control
+                      type="date"
+                      onChange={(e) => setNewDOB(e.target.value)}
+                      size="sm"
+                    />
+                    <Form.Label>Email: </Form.Label>
+                    <Form.Control
+                      onChange={(e) => setNewEmail(e.target.value)}
+                      size="sm"
+                    />
+                    <Form.Label>Phone numer: </Form.Label>
+                    <Form.Control
+                      onChange={(e) => setNewPhoneNum(e.target.value)}
+                      size="sm"
+                    />
+                    <Form.Label>Address: </Form.Label>
+                    <Form.Control
+                      onChange={(e) => setNewAddress(e.target.value)}
+                      size="sm"
+                    />
+                    <Form.Label>City: </Form.Label>
+                    <Form.Control
+                      onChange={(e) => setNewCity(e.target.value)}
+                      size="sm"
+                    />
+                    <Form.Label>Prov: </Form.Label>
+                    <Form.Control
+                      onChange={(e) => setNewProv(e.target.value)}
+                      size="sm"
+                    />
+                    <Form.Label>Postal code: </Form.Label>
+                    <Form.Control
+                      onChange={(e) => setNewPostalCode(e.target.value)}
+                      size="sm"
+                    />
+                    <Form.Label>Alberta Health Care: </Form.Label>
+                    <Form.Control
+                      onChange={(e) => setNewAHCNum(e.target.value)}
+                      size="sm"
+                    />
+                    <Form.Label>Emergecy contact name: </Form.Label>
+                    <Form.Control
+                      onChange={(e) => setNewEmergName(e.target.value)}
+                      size="sm"
+                    />
+                    <Form.Label>Emergency contact number: </Form.Label>
+                    <Form.Control
+                      onChange={(e) => setNewEmergNum(e.target.value)}
+                      size="sm"
+                    />
+                  </Form.Group>
+                  <div className="grid-div">
+                    <div className="confirmChange-div item-2">
+                      <Button className="confirmChange-btn">Create</Button>
+                      <br />
+                      <a className="admin-link" onClick={showUsers}>
+                        Cancel
+                      </a>
+                    </div>
+                  </div>
+                </Form>
+              </div>
             )}
           </div>
         </div>
