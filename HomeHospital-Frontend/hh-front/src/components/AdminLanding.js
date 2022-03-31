bb328824b643c208d757599e114eaf64849c2c9e
rawbz <rmjbalanag@gmail.com>
2022-03-31 10:41:36 -0600

    added pie display

--- HomeHospital-Frontend/hh-front/src/components/AdminLanding.js
+++ HomeHospital-Frontend/hh-front/src/components/AdminLanding.js
@@ -3,37 +3,77 @@ import { AdminContext } from "./AdminContext";
 import "../styles/admin.css";
 import Users from "../data/users.json";
 import { Card } from "react-bootstrap";
+import { ResponsivePie } from "@nivo/pie";
 
 function AdminLanding() {
   const { menuSelection } = useContext(AdminContext);
   // console.log(displayUsers);
   const [menuChoice, setMenuChoice] = menuSelection;
 
+  //piechart to display users
+  const data = [
+    {
+      id: "Patients",
+      label: "Patients",
+      value: 195,
+      color: "hsl(359, 70%, 50%)"
+    },
+    {
+      id: "Practitioners",
+      label: "practitioners",
+      value: 419,
+      color: "hsl(167, 70%, 50%)"
+    },
+    {
+      id: "Admininistrators",
+      label: "Administrators",
+      value: 407,
+      color: "hsl(102, 70%, 50%)"
+    }
+  ];
+
+  const Pie = () => {
+    return (
+      <ResponsivePie
+      data={data}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      innerRadius={0.5}
+      padAngle={0.7}
+      cornerRadius={3}
+      activeOuterRadiusOffset={8}
+      colors={{ scheme: 'purple_red' }}
+      borderWidth={1}
+      borderColor={{
+          from: 'color',
+          modifiers: [
+              [
+                  'darker',
+                  0.2
+              ]
+          ]
+      }}
+        arcLinkLabelsSkipAngle={10}
+        arcLinkLabelsTextColor="#333333"
+        arcLinkLabelsThickness={2}
+        arcLinkLabelsColor={{ from: "color" }}
+        arcLabelsSkipAngle={10}
+        arcLabelsTextColor={{ from: "color", modifiers: [["darker", 2]] }}
+      />
+    );
+  };
+
+
   return (
     <>
       {menuChoice == "" && (
         <>
           <h1 className="adminGreeting">Welcome Robyn!</h1>
-          <div className="landing-grid">
-              <Card className="card-1">
-                  <Card.Title>Current Users</Card.Title>
-                  <Card.Body>
-                        <p>Total Users: {Users.length} </p>
-                  </Card.Body>
-              </Card>
-              <Card className="card-2">
-                  <Card.Title>Current Users</Card.Title>
-                  <Card.Body>
-                        <p>Total Users: {Users.length} </p>
-                  </Card.Body>
-              </Card>
-              <Card className="card-3">
-                  <Card.Title>Current Users</Card.Title>
-                  <Card.Body>
-                        <p>Total Users: {Users.length} </p>
-                  </Card.Body>
-              </Card>
-          </div>
+            <div className="currentUserDisplay">
+            <p>Current user details</p>
+              <div className="responsivePie">
+                <Pie />
+              </div>
+            </div>
         </>
       )}
     </>
@@ -41,3 +81,24 @@ function AdminLanding() {
 }
 
 export default AdminLanding;
+
+          // <div className="landing-grid">
+          //     <Card className="card-1">
+          //         <Card.Title>Current Users</Card.Title>
+          //         <Card.Body>
+          //               <p>Total Users: {Users.length} </p>
+          //         </Card.Body>
+          //     </Card>
+          //     <Card className="card-2">
+          //         <Card.Title>Current Users</Card.Title>
+          //         <Card.Body>
+          //               <p>Total Users: {Users.length} </p>
+          //         </Card.Body>
+          //     </Card>
+          //     <Card className="card-3">
+          //         <Card.Title>Current Users</Card.Title>
+          //         <Card.Body>
+          //               <p>Total Users: {Users.length} </p>
+          //         </Card.Body>
+          //     </Card>
+          // </div>
