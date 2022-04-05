08b3fa19d21075728ab5c27ffe17ddca9581846f
RidgeBnz <ridge.banez@edu.sait.ca>
2022-04-04 21:10:14 -0600

    Update PractionerWaitlist.jsx
    minor update.

--- HomeHospital-Frontend/hh-front/src/components/PractionerWaitlist.jsx
+++ HomeHospital-Frontend/hh-front/src/components/PractionerWaitlist.jsx
@@ -72,8 +72,8 @@ export default function PractionerWaitlist({ childToParent }) {
   };
 
   //check the id, display alert to confirm
-  const handleDelete = (e) => {
-    console.log("this is the id of the user to delete: " + e);
+  const handleCheckIn = (e) => {
+    console.log("this is the id of the user to check in: " + e);
     {
 		practPatientInfo.map((data) => {
         if (practPatientInfo._id === e) {
@@ -108,7 +108,7 @@ export default function PractionerWaitlist({ childToParent }) {
           </Button>
         </td>
         <td>
-          <Button onClick={(e) => handleDelete(practPatientInfo._id)}>Check In</Button>
+          <Button onClick={(e) => handleCheckIn(practPatientInfo._id)}>Check In</Button>
         </td>
       </tr>
     );
