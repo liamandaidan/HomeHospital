61f726af404f456c749f742fde126f18113509d0
Lance Gee <Lancegee.apps@gmail.com>
2022-04-12 20:14:24 -0600

    Update BreadcrumbNav.js

--- HomeHospital-Frontend/hh-front/src/components/BreadcrumbNav.js
+++ HomeHospital-Frontend/hh-front/src/components/BreadcrumbNav.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
+import { Link } from "react-router-dom";
 
 function BreadcrumbNav() {
   return (
     <>
       <div className="breadcrumb-div">
         <Breadcrumb>
-          <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
-          <Breadcrumb.Item href="/hospitals">
+          <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/home" }}>
+            Home
+          </Breadcrumb.Item>
+          <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/hospitals" }}>
             Hospital
           </Breadcrumb.Item>
           <Breadcrumb.Item active>Symptoms</Breadcrumb.Item>
