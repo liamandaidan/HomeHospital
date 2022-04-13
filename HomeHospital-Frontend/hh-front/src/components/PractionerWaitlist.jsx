2d5409460a57ecfc1d445879c9161fb49e9f4909
Liam McLaughlin <84680661+liamandaidan@users.noreply.github.com>
2022-04-13 10:39:51 -0600

    undo

--- HomeHospital-Frontend/hh-front/src/components/PractionerWaitlist.jsx
+++ HomeHospital-Frontend/hh-front/src/components/PractionerWaitlist.jsx
@@ -7,7 +7,7 @@ axios.defaults.withCredentials = true;
 
 export default function PractionerWaitlist({ childToParent, refresh }) {
   //useContext here
-  const { _id, additionalInfo, symptomsInfo, hidden} =
+  const { _id, additionalInfo, symptomsInfo} =
     useContext(PractitionerContext);
 
   //useContext patient id
@@ -15,7 +15,6 @@ export default function PractionerWaitlist({ childToParent, refresh }) {
   //set state for additional info, and symptoms from route and is a useContext
   const [patientAdditionalInfo,setPatientAdditionalInfo] = additionalInfo;
   const [symptomsDetails,setSymptomDetails] = symptomsInfo;
-  const [hiddenDetail, setHiddenDetail] = hidden;
 
   //modal state set to false
   const [modalState, setModalState] = useState(false);
@@ -32,7 +31,7 @@ export default function PractionerWaitlist({ childToParent, refresh }) {
   const [flag, setFlag] = useState(false);
   const [isCheckedIn, setIsCheckedIn] = useState(false);
 
-   
+  const [hidden, setHidden] = useState(true);
 
   /**
    * Here when a select component is updated we will update our url to reflect the changes.
@@ -158,7 +157,12 @@ export default function PractionerWaitlist({ childToParent, refresh }) {
     childToParent(e.patient);
     //sets additionalInfo state and data and is in use with useContext
     setPatientAdditionalInfo(e.additionalInfo);
+<<<<<<< HEAD
 	setHiddenDetail(false);
+=======
+
+	setHidden(s => !s);
+>>>>>>> parent of 5d565ea (Hide comp completed)
   }
 
  
@@ -210,6 +214,7 @@ export default function PractionerWaitlist({ childToParent, refresh }) {
         <div className="form-floating">
           <PractionerHospitalSelect childToParent={updateHospitalState}/>
         </div>
+		{!hidden ? <p>This is a test</p> : null}
       </div>
       <div
         className="table-data"
