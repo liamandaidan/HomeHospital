fbfe66f936a5be167b4d524bdb6d0306a73c1a82
Michael Cann <mikepcannbass@gmail.com>
2022-04-14 14:53:49 -0600

    Added call to clear the DB of ref tokens when the server starts up

--- HomeHospital-Backend/src/loaders/database.js
+++ HomeHospital-Backend/src/loaders/database.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import ENV from '../configure/configure.js'
+import refreshTokensSchema from '../models/refreshTokens.Schema.js'
 
 // Function used to connect to the Database
 const DBConnect = async () => {
@@ -7,6 +8,8 @@ const DBConnect = async () => {
 		// Connect to the database with the connection string from the .env file
 		await mongoose.connect(ENV.MONGO_URI)
 		console.log('🗄  DB Connect was was a success!')
+		// Clear out old Ref tokens When the server starts
+		await refreshTokensSchema.find().deleteMany();
 	} catch (error) {
 		console.error(
 			`THE DB FAILED 🤯 :\n.\n..\n...\n....\n.....\nPlease Check Your DB Connection... \n${error.message}`
