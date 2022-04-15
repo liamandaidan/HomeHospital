0a99cfeed23c152938b602dd55d10b0de7d3b5d5
Liam McLaughlin <84680661+liamandaidan@users.noreply.github.com>
2022-04-14 20:34:17 -0600

    updated doc+cleanup

--- HomeHospital-Frontend/hh-front/src/pages/HospitalSelectionPage.js
+++ HomeHospital-Frontend/hh-front/src/pages/HospitalSelectionPage.js
@@ -2,14 +2,17 @@ import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { HomeHospitalContext } from "../components/HomeHospitalContext";
 import HospitalSelectForm from "../components/HospitalSelectionForm";
-
+/**
+ * @name HospitalSelectionPage
+ * @summary this handles all functionality of selecting a hospital as a user.
+ * @returns html component
+ */
 function HospitalSelectionPage() {
   const navigate = useNavigate();
   const { requestButtonOn } = useContext(HomeHospitalContext);
   const [isThereCurrentRequest, setIsThereCurrentRequest] = requestButtonOn;
 
   useEffect(() => {
-    console.log("in the hospital selection page");
     if (!isThereCurrentRequest) {
       navigate("/home");
     }
